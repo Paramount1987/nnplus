d20eb4f5e58d185c03fbcc2cc6e0a4a40d519a05
Paramount1987 <Paramount7891@gmail.com>
2017-05-29 23:02:15 +0300

    != validation

--- src/js/components/app/index.js
+++ src/js/components/app/index.js
@@ -23,8 +23,16 @@ const APP = {
         const _this = this;
         $('body').on('click', this.ajaxLink, function(e){
             e.preventDefault();
+
+            // validate form
+           if( !_this.formValidate($(this).closest('form'))){
+               return;
+           }
+
            const content = $(this).data('content');
            const bg = $(this).data('bg');
+
+           if(!content) return;
 
            $wrapper.fadeTo( 300,0.01, function() {
                $.get( content, function( data ) {
@@ -70,6 +78,40 @@ const APP = {
         $('body').on('mouseleave', _this.profileLink, function(){
             $(_this.profileLink).removeClass('is-unhover is-hover');
         });
+    },
+
+    formValidate: function($form){
+        const emptyError = 'Поле не заполнено';
+        const passwordError = 'Пароли должны совпадать';
+        let counter = 0;
+        let passwords = [];
+
+        //clear error
+        $('.input-error').remove();
+
+        $form.find('.js-input-v, .js-input-p').each(function(){
+            const el = $(this);
+            const value = el.val();
+
+            if(!value){
+              el.after(`<div class="input-error">${emptyError}</div>`);
+                counter++
+            }
+        });
+
+        // validate equal password
+        $form.find('.js-input-p').each(function(){
+            const el = $(this);
+            passwords.push(el.val());
+        });
+
+        if(passwords[0] !== passwords[1]){
+            $form.find('.js-input-p').after(`<div class="input-error">${passwordError}</div>`);
+            counter++;
+        }
+        //----------------
+
+        return counter ? false : true
     }
 }
 
